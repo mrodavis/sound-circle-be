e1eb2ab6f2c2db0aafceb2367fe19dd05e5d0f3c
Oliver Levine <oliversfphillips@icloud.com>
2025-08-25 06:53:30 -0700

    Auth Fixes

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,23 +7,26 @@ const User = require('../models/user');
 
 const saltRounds = 12;
 
-router.post('/sign-up', asnyc(requestAnimationFrame, res) => {
-    console.log(req.body)
-    try {
-        const userInDatabase = await User.findOne({ username: req.body.username });
-        if(userInDatabase) {
-            return res.status(409).json({ err: 'Username already taken.' });
-        }
-        const user = await User.create({
-            username: req.body.username,
-            hashedPassword: bycrypt.hashSnyc(req.body.password, saltRounds)
-        });
-        const payload = { username: user.username, _id: user._id }
+router.post('/sign-up', async (req, res) => {
+  console.log(req.body)
+  try {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+
+    if (userInDatabase) {
+        return res.status(409).json({ err: 'Username already taken.' });
+    }
+
+    const user = await User.create({
+        username: req.body.username,
+        hashedPassword: bcrypt.hashSync(req.body.password, saltRounds)
+    });
+
+    const payload = { username: user.username, _id: user._id }
 
     const token = jwt.sign({ payload }, process.env.JWT_SECRET);
 
-        res.status(201).json({ token })
-    } catch(err) {
+    res.status(201).json({ token })
+    } catch (err) {
         res.status(400).json({ err: err.message })
     }
 });
