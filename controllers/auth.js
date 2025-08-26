6ae721f62e2d0719cbabd27d422435566fd37f33
Bastian Pethel <bastianpethel360@gmail.com>
2025-08-26 09:22:14 -0400

    added verify-token.js and test-jwt.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const bycrypt = require('bycrypt');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
