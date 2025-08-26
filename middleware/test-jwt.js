fa05524b12b061998e542e855c539d6618ae467f
Bastian Pethel <bastianpethel360@gmail.com>
2025-08-26 10:36:10 -0400

    made folder for middleware

--- middleware/test-jwt.js
+++ middleware/test-jwt.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
-const verifyToken = require('../middleware/verify-token');
+const verifyToken = require('./verify-token');
 const router = express.Router();
 
 router.post('/generate', async (req, res) => {
